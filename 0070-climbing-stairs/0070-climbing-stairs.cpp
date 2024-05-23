1106fae5ba07e9babbb01f4a38d9d51ef4b92c14
barkha yadav <143165059+barkhayadav12@users.noreply.github.com>
2024-05-23 19:20:19 +0530

    Time: 2 ms (47.75%), Space: 7.1 MB (65.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,16 +2,24 @@ class Solution {
 public:
     int climb(int n,int dp[])
     {
-        if(n<=1)
-        {
-            return dp[n]=1;
-        }
+       if(n==0)
+       {
+           return 1;
+       }
+       if(n<0)
+       {
+           return 0;
+       }
         if(dp[n]!=-1)
         {
             return dp[n];
         }
-        dp[n]=climb(n-1,dp)+climb(n-2,dp);
-        return dp[n];
+        int ans=0;
+        for(int i=1;i<=2;i++)
+        {
+            ans=ans+climb(n-i,dp);
+        }
+        return dp[n]=ans;
     }
    
     int climbStairs(int n) {
