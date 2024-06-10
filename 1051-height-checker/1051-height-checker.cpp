c497030b11f38df4d473bb9690fc75d03ed34880
barkha yadav <143165059+barkhayadav12@users.noreply.github.com>
2024-06-10 09:58:21 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (44.04%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>v;
+        v=heights;
+        sort(heights.begin(),heights.end());
+        int cnt=0;
+        for(int i=0;i<heights.size();i++)
+        {
+            if(v[i]!=heights[i])
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
