06a7bf31ab18f6c3099b8437ae567b0fd771cae6
barkha yadav <143165059+barkhayadav12@users.noreply.github.com>
2024-06-30 12:00:15 +0530

    Time: 10 ms (28.39%), Space: 10 MB (96.50%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,19 +9,37 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*>mpp;
-        ListNode*p=head;
-        ListNode*ans=NULL;
-        while(p!=NULL)
+        // unordered_set<ListNode*>mpp;
+        // ListNode*p=head;
+        // ListNode*ans=NULL;
+        // while(p!=NULL)
+        // {
+        //     if(mpp.find(p)!=mpp.end())
+        //     {
+        //         ans=p;
+        //         break;
+        //     }
+        //     mpp.insert(p);
+        //     p=p->next;
+        // }
+        // return ans;
+        ListNode*fast=head;
+        ListNode*slow=head;
+        while(fast!=NULL && fast->next!=NULL)
         {
-            if(mpp.find(p)!=mpp.end())
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)
             {
-                ans=p;
-                break;
+                slow=head;
+                while(fast!=slow)
+                {
+                    fast=fast->next;
+                    slow=slow->next;
+                }
+                return slow;
             }
-            mpp.insert(p);
-            p=p->next;
         }
-        return ans;
+        return NULL;
     }
 };
