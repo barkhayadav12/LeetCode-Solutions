d4e1ab22c0eb575c4513ed8b261f97da4adf880d
barkha yadav <143165059+barkhayadav12@users.noreply.github.com>
2024-07-26 14:37:07 +0530

    Time: 30 ms (96.25%), Space: 243.1 MB (5.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -14,10 +14,10 @@ public:
         {
             return dp[n][sum];
         }
-        if(nums[n-1]<=sum)
+        if(nums[n-1]<=sum) //can pick
         {
             return dp[n][sum]=subset(nums,sum-nums[n-1],n-1,dp)|| subset(nums,sum,n-1,dp);
-        }
+        } //cannot pick
         else if(nums[n-1]>sum)
         {
             return dp[n][sum]=subset(nums,sum,n-1,dp);
@@ -31,6 +31,7 @@ public:
             sum+=nums[i];
         }
         vector<vector<int>>dp(nums.size()+1,vector<int>(sum+1,-1));
+        // if the sum turns out to be an odd number, it cannot into two equal halves
         if(sum%2!=0)
         {
             return false;
@@ -40,3 +41,47 @@ public:
         }
     }
 };
+// class Solution {
+// public:
+//    bool solve(vector<int>arr,int ind,int n,int sum,vector<vector<int>>&dp)
+//     {
+//         //base case
+//         if(sum==0)
+//         {
+//             return true;
+//         }
+//         if(sum<0)
+//         {
+//             return false;
+//         }
+//         if(ind>=n)
+//         {
+//             return false;
+//         }
+//         if(dp[sum][ind]!=-1)
+//         {
+//             return dp[sum][ind];
+//         }
+//         // pick
+//         bool pick=solve(arr,ind+1,n,sum-arr[ind],dp);
+//         bool not_pick=solve(arr,ind+1,n,sum,dp);
+//         return dp[sum][ind]=pick || not_pick;
+        
+//     }
+//     bool canPartition(vector<int>& nums) {
+//         int n=nums.size();
+//         int sum=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             sum+=nums[i];
+//         }
+//         // if odd, not possible
+//         if(sum%2==1)
+//         {
+//             return false;
+//         }
+//         int target=sum/2;
+//         vector<vector<int>>dp(target+1,vector<int>(n+1,-1));
+//         return solve(nums,0,n,target,dp);
+//     }
+// };
